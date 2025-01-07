f18a4e49480e86a8d9d567f40a8b828342f34cb8
siliphong-mac <it@BOOKNEAs-Mac-mini.local>
2025-01-07 17:06:04 +0700

    update blade

--- init.lua
+++ init.lua
@@ -581,3 +581,13 @@ cmp.setup {
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+
+local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+parser_config.blade = {
+    install_info = {
+        url = "https://github.com/EmranMR/tree-sitter-blade",
+        files = { "src/parser.c" },
+        branch = "main",
+    },
+    filetype = "blade",
+}
